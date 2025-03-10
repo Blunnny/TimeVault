601f87ac2f33ed8eb45b51cba2b61c9b5215a894
Blunnny <jah22956@gmail.com>
2025-03-10 21:19:18 +0800

    latest

--- proj1/gh2/GuitarString.java
+++ proj1/gh2/GuitarString.java
@@ -1,55 +1,67 @@
 package gh2;
 
 // TODO: uncomment the following import once you're ready to start this portion
-// import deque.Deque;
+
 // TODO: maybe more imports
+
+import deque.Deque;
+import deque.LinkedListDeque;
 
 //Note: This file will not compile until you complete the Deque implementations
 public class GuitarString {
-    /** Constants. Do not change. In case you're curious, the keyword final
+    /**
+     * Constants. Do not change. In case you're curious, the keyword final
      * means the values cannot be changed at runtime. We'll discuss this and
-     * other topics in lecture on Friday. */
-    private static final int SR = 44100;      // Sampling Rate
-    private static final double DECAY = .996; // energy decay factor
+     * other topics in lecture on Friday.
+     */
+    private static final int SR = 44100;      // 采样率
+    private static final double DECAY = .996; // 能量衰减因子
 
     /* Buffer for storing sound data. */
     // TODO: uncomment the following line once you're ready to start this portion
-    // private Deque<Double> buffer;
+    private Deque<Double> buffer;
 
     /* Create a guitar string of the given frequency.  */
     public GuitarString(double frequency) {
-        // TODO: Create a buffer with capacity = SR / frequency. You'll need to
-        //       cast the result of this division operation into an int. For
-        //       better accuracy, use the Math.round() function before casting.
-        //       Your should initially fill your buffer array with zeros.
+        // 初始化 buffer 为一个 Deque<Double>，容量为 SR / frequency（采样率除以频率），表示一个振动周期的样本数
+        // 用 0.0 填充缓冲区，模拟初始静音状态
+        buffer = new LinkedListDeque<>();
+        int capacity = (int) Math.round(SR / frequency); // 采样率除以频率
+        for (int i = 0; i < capacity; i++) {
+            buffer.addLast(0.0);
+        }
     }
 
 
     /* Pluck the guitar string by replacing the buffer with white noise. */
     public void pluck() {
-        // TODO: Dequeue everything in buffer, and replace with random numbers
-        //       between -0.5 and 0.5. You can get such a number by using:
-        //       double r = Math.random() - 0.5;
-        //
-        //       Make sure that your random numbers are different from each
-        //       other. This does not mean that you need to check that the numbers
-        //       are different from each other. It means you should repeatedly call
-        //       Math.random() - 0.5 to generate new random numbers for each array index.
+        // 模拟拨弦，将 buffer 中的所有元素替换为随机噪声（值在 -0.5 到 0.5 之间）
+        while (!buffer.isEmpty()) {
+            buffer.removeFirst();
+        }
+        int capacity = buffer.size();
+        for (int i = 0; i < capacity; i++) {
+            buffer.addLast(Math.random() - 0.5);
+        }
     }
 
     /* Advance the simulation one time step by performing one iteration of
      * the Karplus-Strong algorithm.
      */
     public void tic() {
-        // TODO: Dequeue the front sample and enqueue a new sample that is
-        //       the average of the two multiplied by the DECAY factor.
-        //       **Do not call StdAudio.play().**
+        // 移除前端样本,与下一个样本平均并衰减,加入末端
+        Double front = buffer.removeFirst();
+        Double newFront = buffer.get(0);
+        double newSample = (front + newFront) * 0.5 * DECAY;
+        buffer.addLast(newSample);
     }
 
     /* Return the double at the front of the buffer. */
     public double sample() {
-        // TODO: Return the correct thing.
-        return 0;
+        if (!buffer.isEmpty()) {
+            return buffer.get(0);
+        }
+        return 0.0;
     }
 }
-    // TODO: Remove all comments that say TODO when you're done.
+// TODO: Remove all comments that say TODO when you're done.
